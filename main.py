b301d08245959e8865f33b2129ddf08f3b2885c1
joka <jokaorgua@users.noreply.github.com>
2024-07-30 08:16:09 +0200

    moved accounts data to separate file for easier development added a workaround for response from playground games is it often gives http code 500

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import requests
 from colorlog import ColoredFormatter
 import uuid
 from utilities import *
+from accountdata import AccountList
 
 # ---------------------------------------------#
 # Configuration
@@ -24,54 +25,7 @@ AccountsRecheckTime = 300
 # For example, if set to 120, the bot will introduce a random delay between 1 and 120 seconds each time it rechecks.
 MaxRandomDelay = 120
 
-# Accounts will be checked in the order they are listed
-AccountList = [
-    {
-        "account_name": "Account 1",  # A custom name for the account (not important, just for logs)
-        "Authorization": "Bearer TOKEN_HERE",  # To get the token, refer to the README.md file
-        "UserAgent": "Your UserAgent",  # Refer to the README.md file to obtain a user agent
-        "Proxy": {},  # You can use proxies to avoid getting banned. Use {} for no proxy
-        # Example of using a proxy:
-        # "Proxy": {
-        #   "https": "https://10.10.1.10:3128",
-        #   "http": "http://user:pass@10.10.1.10:3128/"
-        # },
-        "config": {
-            "auto_tap": True,  # Enable auto tap by setting it to True, or set it to False to disable
-            "auto_free_tap_boost": True,  # Enable auto free tap boost by setting it to True, or set it to False to disable
-            "auto_get_daily_cipher": True,  # Enable auto get daily cipher by setting it to True, or set it to False to disable
-            "auto_get_daily_task": True,  # Enable auto get daily task by setting it to True, or set it to False to disable
-            "auto_get_task": True,  # Enable auto get (Youtube/Twitter and ...) task to True, or set it to False to disable
-            "auto_finish_mini_game": True,  # Enable auto finish mini game by setting it to True, or set it to False to disable
-            "auto_playground_games": False,  # Enable auto playground games by setting it to True, or set it to False to disable
-            # If you have more than 5 accounts, you need to disable the auto_playground_games feature or use a proxy for each account.
-            "auto_upgrade": True,  # Enable auto upgrade by setting it to True, or set it to False to disable
-            "auto_upgrade_start": 2_000_000,  # Start buying upgrades when the balance is greater than this amount
-            "auto_upgrade_min": 100_000,  # Stop buying upgrades when the balance is less than this amount
-            # This feature will ignore the auto_upgrade_start and auto_upgrade_min.
-            # By changing it to True, the bot will first find the overall best card and then wait for the best card to be available (based on cooldown or price).
-            # When the best card is available, the bot will buy it and then wait for the next best card to be available.
-            # This feature will stop buying upgrades when the balance is less than the price of the best card.
-            "wait_for_best_card": False,  # Recommended to keep it True for high level accounts
-            "enable_parallel_upgrades": True,  # Enable parallel card upgrades. This will buy cards in parallel if the best card is on cooldown. It should speed up the profit.
-            "parallel_upgrades_max_price_per_hour": 6_000_000,  # Cards with less than X coins per 1k will be bought
-        },
-        # If you have enabled Telegram bot logging,
-        # you can add your chat ID below to receive logs in your Telegram account.
-        # You can obtain your chat ID by messaging @chatIDrobot.
-        # Example: "telegram_chat_id": "12345678".
-        # If you do not wish to use this feature for this account, leave it empty.
-        # This feature is optional and is required to enable the telegramBotLogging feature below.
-        "telegram_chat_id": "",  # String - you can get it from https://t.me/chatIDrobot
-    },
-    # Add more accounts if you want to use multiple accounts
-    # {
-    #     "account_name": "Account 2",
-    #     "Authorization": "Bearer Token_Here",
-    #     ...
-    #     other configurations like the first account
-    # },
-]
+
 
 SupportedPromoGames = {
     "43e35910-c168-4634-ad4f-52fd764a843f": "Bike Ride 3D in Hamster FAM",
@@ -834,7 +788,7 @@ class HamsterKombatAccount:
         # Send POST request
         response = self.HttpRequest(url, headers, "POST", 200)
 
-        if response is None:
+        if response is None or not response:
             log.error(f"[{self.account_name}] Unable to get playground games.")
             self.SendTelegramLog(
                 f"[{self.account_name}] Unable to get playground games.", "other_errors"
@@ -921,7 +875,7 @@ class HamsterKombatAccount:
         )
 
         response = self.HttpRequest(url, headers, "POST", 200, payload)
-        if response is None:
+        if response is None or not response:
             log.error(
                 f"[{self.account_name}] Unable to get Bike Ride 3D in Hamster FAM key."
             )
