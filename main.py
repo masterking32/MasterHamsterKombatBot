9b17f7640acaabcdd3adb11a67e5df08c8c9a8bc
Amin Mahmoudi <Lichwow_masterking@yahoo.com>
2024-08-01 15:32:05 +0330

    Update main.py

--- main.py
+++ main.py
@@ -40,21 +40,21 @@ AccountList = [
             "auto_tap": True,  # Enable auto tap by setting it to True, or set it to False to disable
             "auto_free_tap_boost": True,  # Enable auto free tap boost by setting it to True, or set it to False to disable
             "auto_get_daily_cipher": True,  # Enable auto get daily cipher by setting it to True, or set it to False to disable
-            "auto_get_daily_task": True,  # Enable auto get daily task by setting it to True, or set it to False to disable
+            "auto_get_daily_task": True,  # Enable auto get daily tasks by setting it to True, or set it to False to disable
             "auto_get_task": True,  # Enable auto get (Youtube/Twitter and ...) task to True, or set it to False to disable
             "auto_finish_mini_game": True,  # Enable auto finish mini game by setting it to True, or set it to False to disable
             "auto_playground_games": False,  # Enable auto playground games by setting it to True, or set it to False to disable
-            # If you have more than 5 accounts, you need to disable the auto_playground_games feature or use a proxy for each account.
-            "auto_upgrade": True,  # Enable auto upgrade by setting it to True, or set it to False to disable
+            # If you have over 5 accounts, disable the auto_playground_games feature or use a proxy for each account.
+            "auto_upgrade": True,  # Enable auto-upgrade by setting it to True, or set it to False to disable
             "auto_upgrade_start": 2_000_000,  # Start buying upgrades when the balance is greater than this amount
             "auto_upgrade_min": 100_000,  # Stop buying upgrades when the balance is less than this amount
             # This feature will ignore the auto_upgrade_start and auto_upgrade_min.
-            # By changing it to True, the bot will first find the overall best card and then wait for the best card to be available (based on cooldown or price).
+            # By changing it to True, the bot will find the overall best card and then wait for the best card to be available (based on cooldown or price).
             # When the best card is available, the bot will buy it and then wait for the next best card to be available.
             # This feature will stop buying upgrades when the balance is less than the price of the best card.
-            "wait_for_best_card": False,  # Recommended to keep it True for high level accounts
+            "wait_for_best_card": True,  # Recommended to keep it True for high-level accounts
             "enable_parallel_upgrades": True,  # Enable parallel card upgrades. This will buy cards in parallel if the best card is on cooldown. It should speed up the profit.
-            "parallel_upgrades_max_price_per_hour": 6_000_000,  # Cards with less than X coins per 1k will be bought
+            "parallel_upgrades_max_price_per_hour": 700,  # Cards with less than X coins per 1k will be bought
         },
         # If you have enabled Telegram bot logging,
         # you can add your chat ID below to receive logs in your Telegram account.
@@ -69,7 +69,7 @@ AccountList = [
     #     "account_name": "Account 2",
     #     "Authorization": "Bearer Token_Here",
     #     ...
-    #     other configurations like the first account
+    #     Other configurations like the first account
     # },
 ]
 
