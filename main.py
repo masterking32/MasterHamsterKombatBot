d7cc97b7a523bee76e9a8168dab78a98123cb25d
Fy0urM <39411717+Fy0urM@users.noreply.github.com>
2024-08-15 19:43:48 +1000

    Added new playground game Merge Away

--- main.py
+++ main.py
@@ -75,6 +75,13 @@ SupportedPromoGames = {
         "delay": 120,
         "retry_delay": 20,
     },
+    "dc128d28-c45b-411c-98ff-ac7726fbaea4": {
+        "name": "Merge Away",
+        "appToken": "8d1cc2ad-e097-4b86-90ef-7a27e19fb833",
+        "promoId": "dc128d28-c45b-411c-98ff-ac7726fbaea4",
+        "delay": 120,
+        "retry_delay": 20,
+    },
 }
 
 
