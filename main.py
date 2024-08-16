92047f9f21b537e7c563c85ec213b6b5d91e4fb1
OSP-Scata <60847538+OSP-Scata@users.noreply.github.com>
2024-08-17 02:01:50 +0400

    Separate file for promo games (#186)
    * Separate file for promo games
    
    Created a separate file for promo games, for easier adding of the new ones.
    
    * fix spacing
    
    * Update main.py
    
    ---------
    
    Co-authored-by: Amin Mahmoudi <Lichwow_masterking@yahoo.com>

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import requests
 from colorlog import ColoredFormatter
 import uuid
 from utilities import *
+from promogames import *
 
 try:
     from config import *
@@ -45,52 +46,6 @@ log.setLevel(LOG_LEVEL)
 log.addHandler(stream)
 # End of configuration
 # ---------------------------------------------#
-
-SupportedPromoGames = {
-    "43e35910-c168-4634-ad4f-52fd764a843f": {
-        "name": "Bike Ride 3D in Hamster FAM",
-        "appToken": "d28721be-fd2d-4b45-869e-9f253b554e50",
-        "promoId": "43e35910-c168-4634-ad4f-52fd764a843f",
-        "delay": 120,
-        "retry_delay": 20,
-    },
-    "fe693b26-b342-4159-8808-15e3ff7f8767": {
-        "name": "My Clone Army",
-        "appToken": "74ee0b5b-775e-4bee-974f-63e7f4d5bacb",
-        "promoId": "fe693b26-b342-4159-8808-15e3ff7f8767",
-        "delay": 120,
-        "retry_delay": 20,
-    },
-    "b4170868-cef0-424f-8eb9-be0622e8e8e3": {
-        "name": "Chain Cube 2024",
-        "appToken": "d1690a07-3780-4068-810f-9b5bbf2931b2",
-        "promoId": "b4170868-cef0-424f-8eb9-be0622e8e8e3",
-        "delay": 120,
-        "retry_delay": 20,
-    },
-    "c4480ac7-e178-4973-8061-9ed5b2e17954": {
-        "name": "Train Miner",
-        "appToken": "82647f43-3f87-402d-88dd-09a90025313f",
-        "promoId": "c4480ac7-e178-4973-8061-9ed5b2e17954",
-        "delay": 120,
-        "retry_delay": 20,
-    },
-    "dc128d28-c45b-411c-98ff-ac7726fbaea4": {
-        "name": "Merge Away",
-        "appToken": "8d1cc2ad-e097-4b86-90ef-7a27e19fb833",
-        "promoId": "dc128d28-c45b-411c-98ff-ac7726fbaea4",
-        "delay": 120,
-        "retry_delay": 20,
-    },
-    "61308365-9d16-4040-8bb0-2f4a4c69074c": {
-        "name": "Twerk Race",
-        "appToken": "61308365-9d16-4040-8bb0-2f4a4c69074c",
-        "promoId": "61308365-9d16-4040-8bb0-2f4a4c69074c",
-        "delay": 120,
-        "retry_delay": 20,
-    },
-}
-
 
 class HamsterKombatAccount:
     def __init__(self, AccountData):
