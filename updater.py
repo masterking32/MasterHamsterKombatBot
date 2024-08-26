944c28fc7797a99221f5d7878946d16648aed788
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-08-26 21:27:50 +0100

    Update updater.py

--- updater.py
+++ updater.py
@@ -2,6 +2,8 @@ import os
 import sys
 import requests
 import time
+import psutil
+import subprocess
 
 # Constants
 FILES = {
@@ -17,6 +19,8 @@ FILES = {
     "utilities.py": "https://raw.githubusercontent.com/masterking32/MasterHamsterKombatBot/main/utilities.py"
 }
 CHECK_DELAY = 10  # Delay in seconds between each update check cycle
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))  # Current directory of the updater script
+MAIN_SCRIPT_PATH = os.path.join(CURRENT_DIR, 'main.py')  # Full path to main.py
 
 def get_local_file_contents(file_path):
     try:
@@ -72,9 +76,26 @@ def download_update(file_name, github_url):
         print(f"General error writing to file {file_name}: {e}")
         return False
 
+def close_main_process():
+    for proc in psutil.process_iter(['pid', 'name', 'cmdline']):
+        cmdline = proc.info['cmdline']
+        if cmdline and proc.info['name'] == 'py.exe' and MAIN_SCRIPT_PATH in cmdline:
+            print(f"Closing process: {proc.info['name']} (PID: {proc.info['pid']}) running {MAIN_SCRIPT_PATH}")
+            proc.terminate()  # Terminate the process
+            proc.wait()  # Wait for the process to terminate
+            break
+
+def reopen_main():
+    try:
+        print("Reopening main.py in a new command window...")
+        subprocess.Popen(['cmd', '/c', 'start', 'python', MAIN_SCRIPT_PATH], shell=True)
+    except Exception as e:
+        print(f"Error reopening main.py: {e}")
+
 def update_check():
     updates_needed = check_for_updates()
     if updates_needed:
+        close_main_process()  # Close main.py process if it's running
         all_updates_successful = True
         for file_name in updates_needed:
             github_url = FILES[file_name]
@@ -82,6 +103,7 @@ def update_check():
                 all_updates_successful = False
         if all_updates_successful:
             print("All updates downloaded.")
+            reopen_main()  # Always reopen main.py after updates
         else:
             print("Some updates failed to download.")
     else:
