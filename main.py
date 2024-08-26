4f76e821866328f58050a819d14c1d895c51be6e
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-08-26 15:55:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 # Date: 2024
 # Github: https://github.com/masterking32
 
+# BEGIN_UPDATE_LOGIC
+from updater import update_check
+update_check()
+# END_UPDATE_LOGIC
 import asyncio
 import datetime
 import json
