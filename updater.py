15a077fb42750d91270d11aad30fdca3404d8c9d
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-08-31 23:07:23 +0100

    Update updater.py

--- updater.py
+++ updater.py
@@ -7,7 +7,7 @@ import subprocess
 import json
 import logging
 from concurrent.futures import ThreadPoolExecutor
-
+######################
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
