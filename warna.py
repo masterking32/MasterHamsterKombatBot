7ad7afbe3185a98c1dc34643953787a0fea9e628
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-09-09 20:09:36 +0100

    Create warna.py

---
+++ warna.py
@@ -0,0 +1,34 @@
+# ansi_colors.py
+
+# Regular Text Colors
+bl = "\033[30m"  # black
+r = "\033[31m"  # red
+g = "\033[32m"  # green
+y = "\033[33m"  # yellow
+b = "\033[34m"  # blue
+m = "\033[35m"  # magenta
+c = "\033[36m"  # cyan
+w = "\033[37m"  # white
+
+# Bright Text Colors
+blt = "\033[90m"  # black_bright
+rt = "\033[91m"  # red_bright
+gt = "\033[92m"  # green_bright
+yt = "\033[93m"  # yellow_bright
+bt = "\033[94m"  # blue_bright
+mt = "\033[95m"  # magenta_bright
+ct = "\033[96m"  # cyan_bright
+wt = "\033[97m"  # white_bright
+
+# Bold Text Colors
+blb = "\033[1;30m"  # black_bold
+rb = "\033[1;31m"  # red_bold
+gb = "\033[1;32m"  # green_bold
+yb = "\033[1;33m"  # yellow_bold
+bb = "\033[1;34m"  # blue_bold
+mb = "\033[1;35m"  # magenta_bold
+cb = "\033[1;36m"  # cyan_bold
+wb = "\033[1;37m"  # white_bold
+
+# Reset Color
+rs = "\033[0m"
