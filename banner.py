9ad0684dcb3c3cb6d1b6fb74f8b100b2deafbf70
copycatcodex <gaarandongdotcom@gmail.com>
2024-09-09 23:50:00 +0700

    i changed the log display on the bot, when it runs. Maybe there are some I missed, sorry

---
+++ banner.py
@@ -0,0 +1,15 @@
+import warna as w
+
+def show_banner():
+    banner = f"""
+    {w.g} _   _                     _           {w.m} _  __               _           _   {w.rs} 
+    {w.g}| | | | __ _ _ __ ___  ___| |_ ___ _ __{w.m}| |/ /___  _ __ ___ | |__   __ _| |_ {w.rs} 
+    {w.g}| |_| |/ _` | '_ ` _ \/ __| __/ _ \ '__{w.m}| ' // _ \| '_ ` _ \| '_ \ / _` | __|{w.rs} 
+    {w.g}|  _  | (_| | | | | | \__ \ ||  __/ |  {w.m}| . \ (_) | | | | | | |_) | (_| | |_ {w.rs} 
+    {w.g}|_| |_|\__,_|_| |_| |_|___/\__\___|_|  {w.m}|_|\_\___/|_| |_| |_|_.__/ \__,_|\__|{w.rs} 
+    {w.yb}Version: 2.3{w.rs} ==================================== Developed by: {w.cb}MasterkinG32{w.rs}
+    
+    Project Github: {w.m}https://github.com/masterking32/MasterHamsterKombatBot{w.rs}
+                            ⛔⛔ {w.rb}CTRL + C to STOP THE BOT !! {w.rs}⛔⛔
+    """
+    print(banner)
